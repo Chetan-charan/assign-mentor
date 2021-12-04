ff2cca3a9c676acc70dd0b532070184e1f8f4218
Chetan-charan <chetanharicharan@gmail.com>
2021-12-04 17:40:45 +0530

    cors

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ import { mentorRouter } from "./routes/mentors.js"
 import cors from "cors";
 
 const app = express()
-app.use(cors()); 
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+app.use(cors(corsOptions)); 
+
 dotenv.config();
+
 const PORT = process.env.PORT;               
 const MONGO_URL = process.env.MONGO_URL;
 
